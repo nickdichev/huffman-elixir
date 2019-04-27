a9ec2fbaaa46b60f08e65de60a6723d36ab05a57
Nick Dichev <ndichev@barracuda.com>
2019-04-26 21:04:35 -0700

    Update dialyxir to be dev/text only

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Huffman.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5.1"},
+      {:dialyxir, "~> 0.5.1", only: [:dev, :test]},
       {:typed_struct, "~> 0.1.4"}
     ]
   end
