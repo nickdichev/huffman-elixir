4843865fcc1e613a6692577a02106fd9600e0b99
Nick Dichev <ndichev@barracuda.com>
2019-04-27 12:41:27 -0700

    Add merge/2 in Huffman.TreeNode

--- lib/huffman/tree_node.ex
+++ lib/huffman/tree_node.ex
@@ -1,6 +1,9 @@
 defmodule Huffman.TreeNode do
   use TypedStruct
+  alias __MODULE__
   @moduledoc false
+
+  @type tree_node() :: %Huffman.TreeNode{}
 
   typedstruct do
     field :character, iodata() | nil, default: nil
@@ -16,10 +19,26 @@ defmodule Huffman.TreeNode do
     iex> Huffman.TreeNode.from_tuple({"a", 3})
     %Huffman.TreeNode{character: "a", weight: 3}
   """
+  @spec from_tuple(tuple()) :: tree_node()
   def from_tuple({character, weight}) do
-    %Huffman.TreeNode{
+    %TreeNode{
       character: character,
       weight: weight
     }
+  end
+
+  @doc """
+  Merges two %Huffman.TreeNode{} into a singular parent %Huffman.TreeNode{}. The parent holds the children in `:left` and `:right`.
+
+  ## Examples
+    iex> left_child = %Huffman.TreeNode{character: "a", weight: 3}
+    ...> right_child = %Huffman.TreeNode{character: "b", weight: 6}
+    ...> Huffman.TreeNode.merge(left_child, right_child)
+    %Huffman.TreeNode{character: nil, weight: 9, left: %Huffman.TreeNode{character: "a", weight: 3}, right: %Huffman.TreeNode{character: "b", weight: 6}}
+  """
+  @spec merge(tree_node(), tree_node()) :: tree_node()
+  def merge(left_child, right_child) do
+    weight = left_child.weight + right_child.weight
+    %TreeNode{weight: weight, left: left_child, right: right_child}
   end
 end
--- test/huffman/tree_node_test.exs
+++ test/huffman/tree_node_test.exs
@@ -1,11 +1,45 @@
 defmodule Huffman.TreeNodeTest do
   use ExUnit.Case, async: true
+  alias Huffman.TreeNode
   doctest Huffman.TreeNode
   @moduledoc false
 
   test "converts from tuple" do
     result = Huffman.TreeNode.from_tuple({"a", 3})
     expected = %Huffman.TreeNode{character: "a", weight: 3}
+    assert expected == result
+  end
+
+  test "merge two leaf nodes" do
+    left_child = %TreeNode{character: "a", weight: 3}
+    right_child = %TreeNode{character: "b", weight: 6}
+
+    result = Huffman.TreeNode.merge(left_child, right_child)
+    expected = %TreeNode{
+      character: nil,
+      weight: 9,
+      left: left_child,
+      right: right_child
+    }
+
+    assert expected == result
+  end
+
+  test "merge leaf node into non-leaf node" do
+    left_child = %TreeNode{character: "a", weight: 3}
+    right_child = %TreeNode{character: "b", weight: 6}
+    parent = Huffman.TreeNode.merge(left_child, right_child)
+
+    right_child = %TreeNode{character: "c", weight: 10}
+    result = Huffman.TreeNode.merge(parent, right_child)
+
+    expected = %TreeNode{
+      character: nil,
+      weight: 19,
+      left: parent,
+      right: right_child
+    }
+
     assert expected == result
   end
 end
