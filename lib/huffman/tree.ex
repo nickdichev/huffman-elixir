a3296d6b562ed82aa18209a91e31027b6f4610db
Nick Dichev <ndichev@barracuda.com>
2019-04-27 20:33:49 -0700

    Implemented Huffman.Tree.inorder/1
    This function completes an inorder traversal of the tree and keeps track
    of the state of the recursion with an Agent. The final state of the
    agent is a map which has characters as the key and the value is an iolist of the
    character's huffman encoding.

--- lib/huffman/tree.ex
+++ lib/huffman/tree.ex
@@ -11,17 +11,53 @@ defmodule Huffman.Tree do
   @spec from_priority_queue(priority_queue()) :: tree_node()
   def from_priority_queue(queue), do: flatten(queue)
 
-  defp flatten([node | []]), do: node
+  # Base of the recursion, there is only one node left -- the root of the Huffman tree
+  defp flatten([root | []]), do: root
 
   defp flatten(queue) do
+    # Pop the two lowest weight nodes off of the queue
     {left_child, queue} = PriorityQueue.pop(queue)
     {right_child, queue} = PriorityQueue.pop(queue)
 
+    # Merge the two nodes into a new node, and put the new node on the queue
     parent = TreeNode.merge(left_child, right_child)
     queue = PriorityQueue.insert(queue, parent)
 
+    # Keep the recursion going
     flatten(queue)
   end
 
+  @doc """
+  Complete a recursive inorder traversal of the Huffman tree. While the recursion is happening
+  the "path" of the recursion is tracked. 0 represents going left in the tree, while 1
+  represents going right in the tree. When the recursion hits the base case, a leaf node, the
+  recursion path is stored for the character that leaf node represents.
+  """
+  @spec inorder(tree_node()) :: map()
+  def inorder(root) do
+    # Start an agent which stores the final encoding for each leaf node
+    # We are only doing simple inserts, so lets not worry about supervision
+    {:ok, agent} = Agent.start(fn -> %{} end)
+
+    # Actually do the recursion, passing in the agent to keep track of state
+    inorder(root, [], agent)
+
+    # Get the final state of the map
+    encoding_map = Agent.get(agent, fn state -> state end)
+
+    # Cleanup then return the map to the caller
+    Agent.stop(agent)
+    encoding_map
+  end
+
+  # Base of the recursion, we are at a leaf node when :left and :right are nil
+  defp inorder(%{left: nil, right: nil} = node, iolist, agent) do
+    Agent.update(agent, &Map.put(&1, node.character, iolist))
+  end
+
+  defp inorder(node, iolist, agent) do
+    inorder(node.left, [iolist, <<0 :: size(1)>>], agent)
+    inorder(node.right, [iolist, <<1 :: size(1)>>], agent)
+  end
 
 end
--- test/huffman/tree_test.exs
+++ test/huffman/tree_test.exs
@@ -1,5 +1,53 @@
 defmodule Huffman.TreeTest do
   use ExUnit.Case, async: true
+  alias Huffman.{Tree, TreeNode}
   @moduledoc false
+
+  setup do
+    queue = [
+      %TreeNode{character: "b", left: nil, right: nil, weight: 1},
+      %TreeNode{character: "c", left: nil, right: nil, weight: 1},
+      %TreeNode{character: "a", left: nil, right: nil, weight: 4},
+      %TreeNode{character: "d", left: nil, right: nil, weight: 10}
+    ]
+    {:ok, queue: queue}
+  end
+
+  test "creates tree from priority queue", %{queue: queue} do
+    root = Tree.from_priority_queue(queue)
+
+    expected = %TreeNode{
+      character: nil,
+      left: %TreeNode{
+        character: nil,
+        left: %TreeNode{
+          character: nil,
+          left: %TreeNode{character: "b", left: nil, right: nil, weight: 1},
+          right: %TreeNode{character: "c", left: nil, right: nil, weight: 1},
+          weight: 2
+        },
+        right: %TreeNode{character: "a", left: nil, right: nil, weight: 4},
+        weight: 6
+      },
+      right: %TreeNode{character: "d", left: nil, right: nil, weight: 10},
+      weight: 16
+    }
+
+    assert expected == root
+  end
+
+  test "gets encodings from a Huffman tree", %{queue: queue} do
+    root = Tree.from_priority_queue(queue)
+    encodings = Tree.inorder(root)
+
+    expected = %{
+      "a" => [[[], <<0::size(1)>>], <<1::size(1)>>],
+      "b" => [[[[], <<0::size(1)>>], <<0::size(1)>>], <<0::size(1)>>],
+      "c" => [[[[], <<0::size(1)>>], <<0::size(1)>>], <<1::size(1)>>],
+      "d" => [[], <<1::size(1)>>]
+    }
+
+    assert expected == encodings
+  end
 
 end
