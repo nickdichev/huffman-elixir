1d4037737a1dc8c1f5621cc1f8c9418b424bf8dd
Nick Dichev <ndichev@barracuda.com>
2019-05-02 22:24:22 -0700

    Improve documentation in Huffman.Header

--- lib/huffman/header.ex
+++ lib/huffman/header.ex
@@ -1,6 +1,10 @@
 defmodule Huffman.Header do
   @moduledoc false
 
+  @doc """
+  Converts a given character count map into a erlang term binary. This data is the header that
+  is used for decompression.
+  """
   @spec get_header(any()) :: {bitstring(), non_neg_integer()}
   def get_header(char_counts) do
     # We will use `term_to_binary/2` to generate our header. The header will be used for
@@ -10,6 +14,10 @@ defmodule Huffman.Header do
     {binary_term, term_len}
   end
 
+  @doc """
+  Converts an erlang term binary to an term. We expect this to recreate the character map
+  when we need to decompress a file that was compressed by this program.
+  """
   @spec from_binary(binary()) :: any()
   def from_binary(binary) do
     :erlang.binary_to_term(binary)
