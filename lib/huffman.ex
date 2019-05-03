6c3426e23e400df321aced86388a5d63f582c86b
Nick Dichev <ndichev@barracuda.com>
2019-05-02 22:20:58 -0700

    Improved documentation in Huffman

--- lib/huffman.ex
+++ lib/huffman.ex
@@ -5,7 +5,10 @@ defmodule Huffman do
   @header_length 32
   @bits_per_byte 8
 
-  @spec compress_file(binary()) :: :ok
+  @doc """
+  Compresses an input file. The output file is the original filename with ".hf" appended.
+  """
+  @spec compress_file(binary()) :: :ok | {:error, :file.posix()}
   def compress_file(filename) when is_binary(filename) do
     compressed_data =
       filename
@@ -15,8 +18,16 @@ defmodule Huffman do
     File.write!(filename <> ".hf", compressed_data)
   end
 
+  @doc """
+  Compresses an input binary. An iolist of the compressed output is returned.
+  """
+  @spec compress(binary()) :: list(binary())
   def compress(bin) when is_binary(bin), do: compress([bin])
 
+  @doc """
+  Compresses an input iolist. An iolist of the compressed output is returned.
+  """
+  @spec compress(iolist() | %File.Stream{}) :: list(binary())
   def compress(iolist) do
     # Get the character counts for the input. Used to write the header and to build the tree
     # We also need to add the EOF so we can get an encoding for it, and store it in the header
@@ -59,6 +70,10 @@ defmodule Huffman do
     |> Stream.flat_map(&List.flatten/1)
   end
 
+  @doc """
+  Decompresses an input file that should have been compressed by this program. The output file
+  name is the input filename with ".orig" appended.
+  """
   @spec decompress_file(binary()) :: :ok
   def decompress_file(filename) do
     decompressed_data =
@@ -69,11 +84,19 @@ defmodule Huffman do
     File.write!(filename <> ".orig", decompressed_data)
   end
 
+  @doc """
+  Decompresses an input iolist. An iolist of the decompressed output is returned.
+  """
+  @spec decompress(list(binary())) :: iolist()
   def decompress([header_bytes, header, iodata, eof] = iolist) when is_list(iolist) do
     body_binary = IO.iodata_to_binary(iodata)
     decompress(header_bytes <> header <> body_binary <> eof)
   end
 
+  @doc """
+  Decompresses an input binary. An iolist of the decompressed output is returned.
+  """
+  @spec decompress(binary()) :: iolist()
   def decompress(<<header_bytes::size(@header_length), rest::binary>>) do
     header_bit_len = header_bytes * @bits_per_byte
     <<header::size(header_bit_len), body::binary>> = rest
@@ -88,18 +111,22 @@ defmodule Huffman do
     decompressed_output(body, root, root, [])
   end
 
+  # Final base case of decompression, at a leaf node that is the EOF character.
    defp decompressed_output(_rest, _root, %{left: nil, right: nil, character: <<255>>}, iolist) do
      iolist
    end
 
+   # Base case of decompression, append the character of the current leaf node to the iolist.
   defp decompressed_output(rest, root, %{left: nil, right: nil} = node, iolist) do
     decompressed_output(rest, root, root, [iolist, node.character])
   end
 
+  # Consume a 1 off the compressed encoding and go right for the next recursion
   defp decompressed_output(<<1::size(1), rest::bitstring>>, root, node, iolist) do
     decompressed_output(rest, root, node.right, iolist)
   end
 
+  # Consume a 0 off the compressed encoding and go left for the next recursion
   defp decompressed_output(<<0::size(1), rest::bitstring>>, root, node, iolist) do
     decompressed_output(rest, root, node.left, iolist)
   end
