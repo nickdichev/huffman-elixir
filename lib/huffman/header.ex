bef74a457d0a6278c8cb5127d45b95afbb960e46
Nick Dichev <ndichev@barracuda.com>
2019-05-02 20:58:00 -0700

    Add typespecs to Huffman.Header module

--- lib/huffman/header.ex
+++ lib/huffman/header.ex
@@ -1,6 +1,7 @@
 defmodule Huffman.Header do
   @moduledoc false
 
+  @spec get_header(any()) :: {bitstring(), non_neg_integer()}
   def get_header(char_counts) do
     # We will use `term_to_binary/2` to generate our header. The header will be used for
     # decompression. The header will be read and the Huffman tree can be recreated.
@@ -9,6 +10,7 @@ defmodule Huffman.Header do
     {binary_term, term_len}
   end
 
+  @spec from_binary(binary()) :: any()
   def from_binary(binary) do
     :erlang.binary_to_term(binary)
   end
