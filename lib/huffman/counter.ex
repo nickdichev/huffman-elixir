b997b869fc115ad087474ee313f67ae8c7f6ee1a
Nick Dichev <ndichev@barracuda.com>
2019-05-08 21:16:59 -0700

    Consolidate typespec of Huffman.Counter.count/1

--- lib/huffman/counter.ex
+++ lib/huffman/counter.ex
@@ -14,20 +14,20 @@ defmodule Huffman.Counter do
     iex> Huffman.Counter.count([])
     %{}
   """
-  @spec count(iodata() | %File.Stream{}) :: map()
+  @spec count(list(binary) | %File.Stream{}) :: map()
   def count([]), do: %{}
-  def count(iodata) do
-    iodata
+
+  def count(binaries) do
+    binaries
     |> Stream.map(&String.split(&1, ""))
     |> Stream.map(&Enum.filter(&1, fn x -> x != "" end))
     |> Stream.map(&count_helper(&1, %{}))
     |> Enum.reduce(&merge_maps(&1, &2))
   end
 
-  @spec count_helper([], map()) :: map()
+  @spec count_helper(list(binary) | list(), map()) :: map()
   defp count_helper([], acc), do: acc
 
-  @spec count_helper([iodata(), ...], map()) :: map()
   defp count_helper([head | tail], acc) do
     acc = Map.update(acc, head, 1, &(&1 + 1))
     count_helper(tail, acc)
