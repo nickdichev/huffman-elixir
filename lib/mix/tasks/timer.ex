ad7d9575fdc4a2936768a761095114db511ae941
Nick Dichev <nick@dichev.email>
2020-06-05 20:15:48 -0700

    Add ability to call count_flow from timer mix task

--- lib/mix/tasks/timer.ex
+++ lib/mix/tasks/timer.ex
@@ -2,12 +2,17 @@ defmodule Mix.Tasks.Timer do
   use Mix.Task
 
   @shortdoc "Times how long it takes to compress"
-  def run([path]) do
+  def run([path, with_flow?]) do
+    counter_opts = counter_opts(with_flow?)
+
     start_time = System.monotonic_time(:millisecond)
-    _ = Huffman.compress_file(path)
+    _ = Huffman.compress_file(path, counter_opts)
     end_time = System.monotonic_time(:millisecond)
     delta = end_time - start_time
 
     IO.puts("Took #{delta} milliseconds to compress #{Path.basename(path)}")
   end
+
+  defp counter_opts("true"), do: [flow?: true]
+  defp counter_opts(_), do: []
 end
